d2a148eab6212283d29a853639bb0fdba110df2d
TRAVIS ABENDSHIEN <8000602355@wce111-15.local>
2019-01-24 19:07:36 -0800

    Initial Commit

---
+++ Tipper 3000/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper 3000
+//
+//  Created by TRAVIS ABENDSHIEN on 1/24/19.
+//  Copyright © 2019 TRAVIS ABENDSHIEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
