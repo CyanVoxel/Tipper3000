05e17e0129a891dc725aea6d3e35181fc1d560e6
Travis Abendshien <cyanvoxel@Traviss-Mac.local>
2019-01-27 15:36:10 -0800

    -Updated UI Look -Added Outlets and Actions for UI elements -Added tip calculation func and attached to UI

--- Tipper 3000/ViewController.swift
+++ Tipper 3000/ViewController.swift
@@ -15,6 +15,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    //Tip Control Changed
+    
+    //On updated Bill Amount
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Gets Bill amount as double
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calculate tip and total amounts
+        let tipPercentages = [0.1, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex] //tipControl.selectedSegmentIndex
+        let total = bill + tip
+        
+        //Update text labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    //Tap background
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    
+    
 }
 
